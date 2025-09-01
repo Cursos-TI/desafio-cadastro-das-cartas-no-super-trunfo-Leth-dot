122f72cbe902e93809a46a31a40498be8446184d
Leth-dot <202507318616@alunos.estacio.br>
2025-09-01 12:44:51 -0300

    Update cartasupertriunfo.c
    Nivel Mestre - Atualizações

--- cartasupertriunfo.c
+++ cartasupertriunfo.c
@@ -3,12 +3,16 @@
 
 int main(){
 
-    //iniciando inserçao das variaveis de ambas cartas; cada carta possui sua variavel para evitar que o resultado do primeiro sobescreva o segundo
+    //nivel novato - iniciando inserçao das variaveis de ambas cartas; cada carta possui sua variavel para evitar que o resultado do primeiro sobescreva o segundo
     char estado1, estado2, codigo1[20], codigo2[20], nome1[20], nome2[20];
     float area1, area2, pib1, pib2, pibpp1, pibpp2, densidade1, densidade2;
-    int populacao1, populacao2, turisticos1, turisticos2; 
+    float superpoder1, superpoder2; 
+    int turisticos1, turisticos2;
+    int vencedorH, vencedorA, vencedorP, vencedorT, vencedorD, vencedorPP, vencedorS; //nivel mestre - variaveis dos resultados da comparacao simples das cartas
+    unsigned long int populacao1, populacao2;
+    
 
-    //iniciando entrada e saida de dados; carta 01
+    //nives novato - iniciando entrada e saida de dados; carta 01a
     printf("- Carta 01 -\n");
     printf("Represente seu Estado com uma letra de 'A' a 'H': ");
     scanf(" %c", &estado1); 
@@ -20,7 +24,7 @@ int main(){
     scanf(" %s", nome1);
 
     printf("Represente o Numero de Habitantes de sua Cidade: ");
-    scanf(" %d", &populacao1);
+    scanf(" %ld", &populacao1);
 
     printf("Represente o valor da Area de sua Cidade: ");
     scanf(" %f", &area1);
@@ -34,8 +38,11 @@ int main(){
     //nivel aventureiro - atribuicoes as variaveis anteriores para o calculo de pib per capita e densidade - carta 01
     pibpp1 = pib1 / populacao1;
     densidade1 = populacao1 / area1; 
+    //nivel mestre - atribuicao a variavel superpoder1 para ter a soma dos numeros escolhidos pelo usuario (com o inverso da densidade) - carta 01
+    superpoder1 = (float) (populacao1 + area1 + pib1 + turisticos1 + pibpp1) - densidade1;
+     
 
-    //iniciando processo de impressao dos dados; carta 01
+    //nivel novato - iniciando processo de impressao dos dados; carta 01
     printf("\n");
     printf("Estado: %c\n", estado1);
     printf("Codigo: %s\n", codigo1);
@@ -46,9 +53,10 @@ int main(){
     printf("Numero de Pontos Turisticos: %d\n", turisticos1);
     printf("Densidade Populacional: %.2f hab/km²\n", densidade1);
     printf("PIB per capita: %.2f reais\n", pibpp1); 
+    printf("Super Poder: %.2f\n", superpoder1); 
 
 
-    //iniciando entrada e saida de dados; carta 02
+    //nivel novato - iniciando entrada e saida de dados; carta 02
     printf("\n- Carta 02 -\n");
     printf("Represente seu Estado com uma letra de 'A' a 'H': ");
     scanf(" %c", &estado2); 
@@ -60,7 +68,7 @@ int main(){
     scanf(" %s", nome2);
 
     printf("Represente o Numero de Habitantes de sua Cidade: ");
-    scanf(" %d", &populacao2);
+    scanf(" %ld", &populacao2);
 
     printf("Represente o valor da Area de sua Cidade: ");
     scanf(" %f", &area2);
@@ -74,8 +82,10 @@ int main(){
     //nivel aventureiro - atribuicoes as variaveis anteriores para o calculo de pib per capita e densidade - carta 02
     pibpp2 = pib2 / populacao2;
     densidade2 = populacao2 / area2; 
+    //nivel mestre - atribuicao a variavel superpoder1 para ter a soma dos numeros escolhidos pelo usuario (com o inverso da densidade) - carta 02
+    superpoder2 = (float) (populacao2 + area2 + pib2 + turisticos2 + pibpp2) - densidade2;
 
-    //iniciando processo de impressao dos dados; carta 02
+    //nivel novato - iniciando processo de impressao dos dados; carta 02
     printf("\n");
     printf("Estado: %c\n", estado2);
     printf("Codigo: %s\n", codigo2);
@@ -86,8 +96,39 @@ int main(){
     printf("Numero de Pontos Turisticos: %d\n", turisticos2);
     printf("Densidade Populacional: %.2f hab/km²\n", densidade2);
     printf("PIB per capita: %.2f reais\n", pibpp2); 
+    printf("Super Poder: %.2f\n", superpoder2);
+
+    //nivel novato - nivel mestre - comparacao simples das cartas 
+    printf("\n");
+    printf("- Quem venceu? Se apareceu o digito 1, parabens, o Jogador 01 venceu! Se apareceu o digito 0, parabens, o Jogador 02 venceu!-\n\n");
+
+    vencedorH = populacao1 > populacao2; 
+    printf("Numero de Habitantes: %d\n", vencedorH); 
+
+    vencedorA = area1 > area2; 
+    printf("Area: %d\n", vencedorA); 
+
+    vencedorP = pib1 > pib2; 
+    printf("PIB: %d\n", vencedorP); 
+
+    vencedorT = turisticos1 > turisticos2; 
+    printf("Numero de Pontos Turisticos: %d\n", vencedorT); 
+
+    vencedorD = densidade1 < densidade2; 
+    printf("Densidade Populacional: %d\n", vencedorD); 
+
+    vencedorPP = pibpp1 > pibpp2; 
+    printf("PIB per capita: %d\n", vencedorPP); 
+
+    vencedorS = superpoder1 > superpoder2; 
+    printf("Super Poder: %d\n", vencedorS); 
+
+    
+    printf("\nFim de Jogo! Parabens aos vencedores!");
 
 
     return 0;
+}
+
 
 }
